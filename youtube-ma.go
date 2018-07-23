83156ac091fef0b9be6d2618eb729d4238a9bd5f
Corentin Barreau <corentin.barreau@epitech.eu>
2018-07-23 23:58:09 +0200

    Add: fetchingAnnotations - Fetch annotations for an ID on an API-less service.

--- youtube-ma.go
+++ youtube-ma.go
@@ -13,48 +13,74 @@ type Video struct {
 	Title       string
 	Author      string
 	Annotations string
-	ThumbURL    string
+	Thumbnail   string
 }
 
-func fetchingBasic(id string) {
-	// Declaring jq operations
+func fetchingBasic(id string) *Video {
+	video := new(Video)
+	// Declare jq operations
 	getTitle, _ := jq.Parse(".title")
 	getAuthor, _ := jq.Parse(".author_name")
 	getThumb, _ := jq.Parse(".thumbnail_url")
 	// Requesting data from oembed (allow getting title, author, thumbnail url)
 	resp, err := http.Get("https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=" + id + "&format=json")
 	if err != nil {
-		// handle err
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
 	}
 	defer resp.Body.Close()
 	// Checking response status code
 	if resp.StatusCode == http.StatusOK {
 		bodyBytes, err2 := ioutil.ReadAll(resp.Body)
 		if err2 != nil {
-			// handle err
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			os.Exit(1)
 		}
 		bodyString := string(bodyBytes)
 		output := []byte(bodyString)
 		// Parsing data
 		title, _ := getTitle.Apply(output)
+		video.Title = string(title)
 		authorName, _ := getAuthor.Apply(output)
+		video.Author = string(authorName)
 		thumbnailUrl, _ := getThumb.Apply(output)
-		fmt.Println("\nTitle: " + string(title))
-		fmt.Println("Author: " + string(authorName))
-		fmt.Println("Thumbnail: " + string(thumbnailUrl))
+		video.Thumbnail = string(thumbnailUrl)
 	}
-	//author=$(curl -s "https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=${id}&format=json" | jq -r '.author_name')
-	//thumb_url=$(curl -s "https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=${id}&format=json" | jq -r '.thumbnail_url')
-	//annotations=$(curl -s "https://www.youtube.com/annotations_invideo?features=1&legacy=1&video_id=${id}")
+	return video
+}
 
+func fetchingAnnotations(id string, video *Video) *Video {
+	// Requesting annotations from YouTube
+	resp, err := http.Get("https://www.youtube.com/annotations_invideo?features=1&legacy=1&video_id=" + id)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	// Checking response status code
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err2 := ioutil.ReadAll(resp.Body)
+		if err2 != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			os.Exit(1)
+		}
+		annotations := string(bodyBytes)
+		video.Annotations = annotations
+	}
+	return video
 }
 
 func main() {
+	video := new(Video)
 	args := os.Args[1:]
 	id := args[0]
 	key := args[1]
-	fmt.Println("ID: " + id)
-	fmt.Println("API key: " + key)
-	fetchingBasic(id)
-
+	fmt.Println("[DEBUG] ID: " + id)
+	fmt.Println("[DEBUG] API key: " + key)
+	video = fetchingBasic(id)
+	fmt.Println("\nTitle: " + video.Title)
+	fmt.Println("Author: " + video.Author)
+	fmt.Println("Thumbnail: " + video.Thumbnail)
+	video = fetchingAnnotations(id, video)
+	fmt.Println("Annotations: " + video.Annotations)
 }
