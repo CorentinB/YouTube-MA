4809cdbe38a941d1bb1d3acb23d4e9829a3ff33d
Corentin Barreau <corentin.barreau@epitech.eu>
2018-07-28 04:56:31 +0200

    Add: addFormats - Add proper Formats fields to the JSON for all formats available.

--- youtube-ma.go
+++ youtube-ma.go
@@ -18,11 +18,12 @@ import (
 	"sync"
 	"time"
 
+	"net/url"
+
 	"github.com/PuerkitoBio/goquery"
 	"github.com/labstack/gommon/color"
 	"github.com/spf13/cast"
 	"golang.org/x/net/html"
-	"net/url"
 )
 
 // Video structure containing all metadata for the video
@@ -53,32 +54,30 @@ type Track struct {
 
 // infoJSON structure containing the generated json data
 type infoJSON struct {
-	ID                string                `json:"id"`
-	Uploader          string                `json:"uploader"`
-	UploaderID        string                `json:"uploader_id"`
-	UploaderURL       string                `json:"uploader_url"`
-	UploadDate        string                `json:"upload_date"`
-	License           string                `json:"license"`
-	Creator           string                `json:"creator"`
-	Title             string                `json:"title"`
-	AltTitle          string                `json:"alt_title"`
-	Thumbnail         string                `json:"thumbnail"`
-	Description       string                `json:"description"`
-	Category          string                `json:"category"`
-	Tags              []string              `json:"tags"`
-	Subtitles         map[string][]Subtitle `json:"subtitles"`
-	AutomaticCaptions string                `json:"automatic_captions"`
-	Duration          float64               `json:"duration"`
-	AgeLimit          float64               `json:"age_limit"`
-	Annotations       string                `json:"annotations"`
-	Chapters          string                `json:"chapters"`
-	WebpageURL        string                `json:"webpage_url"`
-	ViewCount         float64               `json:"view_count"`
-	LikeCount         float64               `json:"like_count"`
-	DislikeCount      float64               `json:"dislike_count"`
-	AverageRating     float64               `json:"average_rating"`
-	Formats           []Format              `json:"formats"`
-	subLock           sync.Mutex
+	ID            string                `json:"id"`
+	Uploader      string                `json:"uploader"`
+	UploaderID    string                `json:"uploader_id"`
+	UploaderURL   string                `json:"uploader_url"`
+	UploadDate    string                `json:"upload_date"`
+	License       string                `json:"license,omitempty"`
+	Creator       string                `json:"creator,omitempty"`
+	Title         string                `json:"title"`
+	AltTitle      string                `json:"alt_title,omitempty"`
+	Thumbnail     string                `json:"thumbnail"`
+	Description   string                `json:"description"`
+	Category      string                `json:"category"`
+	Tags          []string              `json:"tags"`
+	Subtitles     map[string][]Subtitle `json:"subtitles"`
+	Duration      float64               `json:"duration"`
+	AgeLimit      float64               `json:"age_limit"`
+	Annotations   string                `json:"annotations"`
+	WebpageURL    string                `json:"webpage_url"`
+	ViewCount     float64               `json:"view_count"`
+	LikeCount     float64               `json:"like_count"`
+	DislikeCount  float64               `json:"dislike_count"`
+	AverageRating float64               `json:"average_rating"`
+	Formats       []Format              `json:"formats"`
+	subLock       sync.Mutex
 }
 
 type Subtitle struct {
@@ -88,37 +87,24 @@ type Subtitle struct {
 
 // Format structure for all different formats informations
 type Format struct {
-	FormatID          string          `json:"format_id"`
-	URL               string          `json:"url"`
-	PlayerURL         string          `json:"player_URL"`
-	Ext               string          `json:"ext"`
-	Height            float64         `json:"height"`
-	FormatNote        string          `json:"format_note"`
-	Acodec            string          `json:"acodec"`
-	Abr               float64         `json:"abr"`
-	FileSize          float64         `json:"filesize"`
-	Tbr               float64         `json:"tbr"`
-	Width             float64         `json:"width"`
-	Fps               float64         `json:"fps"`
-	Vcodec            string          `json:"vcodec"`
-	DownloaderOptions []DownloaderOpt `json:"downloader_options"`
-	Format            string          `json:"format"`
-	Protocol          string          `json:"protocol"`
-	HTTPHeader        []HTTPHeaders   `json:"http_headers"`
-}
-
-// DownloaderOpt structure containing downloader options
-type DownloaderOpt struct {
-	HttpChunkSize float64 `json:"http_chunk_size"`
-}
-
-// HTTPHeaders containing the HTTPHeader for each format of video
-type HTTPHeaders struct {
-	UserAgent      string `json:"User-Agent"`
-	AcceptCharset  string `json:"Accept-Charset"`
-	Accept         string `json:"Accept"`
-	AcceptEncoding string `json:"Accept-Encoding"`
-	AcceptLanguage string `json:"Accept-Language"`
+	FormatID     string  `json:"format_id"`
+	Ext          string  `json:"ext"`
+	URL          string  `json:"url"`
+	Height       float64 `json:"height,omitempty"`
+	Width        float64 `json:"width,omitempty"`
+	FormatNote   string  `json:"format_note"`
+	Bitrate      float64 `json:"bitrate"`
+	Fps          float64 `json:"fps,omitempty"`
+	Format       string  `json:"format"`
+	Clen         float64 `json:"clen,omitempty"`
+	EOTF         string  `json:"eotf,omitempty"`
+	Index        string  `json:"index,omitempty"`
+	Init         string  `json:"init,omitempty"`
+	Lmt          float64 `json:"lmt,omitempty"`
+	Primaries    string  `json:"primaries,omitempty"`
+	QualityLabel string  `json:"quality_label,omitempty"`
+	Type         string  `json:"type"`
+	Size         string  `json:"size,omitempty"`
 }
 
 func fetchAnnotations(video *Video, wg *sync.WaitGroup) {
@@ -321,6 +307,82 @@ func parseAverageRating(video *Video, wg *sync.WaitGroup) {
 	}
 }
 
+func addFormats(video *Video) {
+	for _, rawFormat := range video.RawFormats {
+		tmpFormat := Format{}
+		for k, v := range rawFormat {
+			switch k {
+			case "bitrate":
+				tmpFormat.Bitrate, _ = strconv.ParseFloat(v[0], 64)
+			case "clen":
+				tmpFormat.Clen, _ = strconv.ParseFloat(v[0], 64)
+			case "eotf":
+				tmpFormat.EOTF = v[0]
+			case "fps":
+				tmpFormat.Fps, _ = strconv.ParseFloat(v[0], 64)
+			case "index":
+				tmpFormat.Index = v[0]
+			case "init":
+				tmpFormat.Init = v[0]
+			case "itag":
+				tmpFormat.FormatID = v[0]
+				if v[0] == "82" || v[0] == "83" || v[0] == "84" ||
+					v[0] == "85" || v[0] == "100" || v[0] == "101" ||
+					v[0] == "102" {
+					tmpFormat.FormatNote = "3D"
+					tmpFormat.Format = tmpFormat.FormatID + " - " + tmpFormat.FormatNote
+				} else if v[0] == "91" || v[0] == "92" ||
+					v[0] == "93" || v[0] == "94" || v[0] == "95" ||
+					v[0] == "96" || v[0] == "132" || v[0] == "151" {
+					tmpFormat.FormatNote = "HLS"
+					tmpFormat.Format = tmpFormat.FormatID + " - " + tmpFormat.FormatNote
+				} else if v[0] == "139" || v[0] == "140" ||
+					v[0] == "141" || v[0] == "256" || v[0] == "258" ||
+					v[0] == "325" || v[0] == "328" || v[0] == "249" ||
+					v[0] == "250" || v[0] == "251" {
+					tmpFormat.FormatNote = "DASH audio"
+					tmpFormat.Format = tmpFormat.FormatID + " - " + tmpFormat.FormatNote
+				} else if v[0] == "133" || v[0] == "134" ||
+					v[0] == "135" || v[0] == "136" || v[0] == "137" ||
+					v[0] == "138" || v[0] == "160" || v[0] == "212" ||
+					v[0] == "264" || v[0] == "298" || v[0] == "299" ||
+					v[0] == "266" || v[0] == "167" || v[0] == "168" ||
+					v[0] == "169" || v[0] == "170" || v[0] == "218" ||
+					v[0] == "219" || v[0] == "278" || v[0] == "242" ||
+					v[0] == "245" || v[0] == "244" || v[0] == "243" ||
+					v[0] == "246" || v[0] == "247" || v[0] == "248" ||
+					v[0] == "271" || v[0] == "272" || v[0] == "302" ||
+					v[0] == "303" || v[0] == "308" || v[0] == "313" ||
+					v[0] == "315" {
+					tmpFormat.FormatNote = "DASH video"
+					tmpFormat.Format = tmpFormat.FormatID + " - " + tmpFormat.FormatNote
+				} else {
+					tmpFormat.Format = tmpFormat.FormatID + " - " + tmpFormat.Type
+				}
+			case "lmt":
+				tmpFormat.Lmt, _ = strconv.ParseFloat(v[0], 64)
+			case "primaries":
+				tmpFormat.Primaries = v[0]
+			case "quality_label":
+				tmpFormat.QualityLabel = v[0]
+			case "size":
+				tmpFormat.Size = v[0]
+				sizes := strings.Split(v[0], "x")
+				tmpFormat.Width, _ = strconv.ParseFloat(sizes[0], 64)
+				tmpFormat.Height, _ = strconv.ParseFloat(sizes[1], 64)
+			case "type":
+				tmpFormat.Type = v[0]
+				s := strings.Index(v[0], "/")
+				e := strings.Index(v[0], ";")
+				tmpFormat.Ext = v[0][s+1 : e]
+			case "url":
+				tmpFormat.URL = v[0]
+			}
+		}
+		video.InfoJSON.Formats = append(video.InfoJSON.Formats, tmpFormat)
+	}
+}
+
 func parseFormats(video *Video, wg *sync.WaitGroup) {
 	defer wg.Done()
 	if l, ok := video.playerArgs["adaptive_fmts"]; ok {
@@ -330,6 +392,7 @@ func parseFormats(video *Video, wg *sync.WaitGroup) {
 			video.RawFormats = append(video.RawFormats, args)
 		}
 	}
+	addFormats(video)
 }
 
 func parseTags(video *Video, document *goquery.Document, wg *sync.WaitGroup) {
